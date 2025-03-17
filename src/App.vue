0c99e0ed90720e35e76151926bb7d247baf69355
RomanOlsen <romanjolsen@gmail.com>
2025-03-17 16:25:20 -0600

    understanding computed - its like an observer

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@ import { computed, onMounted } from 'vue'
 import { AppState } from './AppState.js'
 import { moonService } from './services/MoonService.js';
 
+const clickUpgrades = AppState.clickUpgrades
+const autoUpgrades = AppState.automaticUpgrades
+const cheese = computed(() => AppState.cheese)
+
+
 onMounted(() => {
   setInterval(moonService.autoMine, 1000);
 })
@@ -15,6 +20,7 @@ function mine() {
 function purchaseUpgrade(upgradeType) {
   moonService.purchaseUpgrade(upgradeType)
 }
+
 
 </script>
 
@@ -32,20 +38,23 @@ function purchaseUpgrade(upgradeType) {
       </div>
       <div class="col-12">
         <div class="d-flex justify-content-center mb-5">
-          <span>{{ AppState.cheese }} Cheese</span>
+          <span>{{ cheese }} Cheese</span>
         </div>
       </div>
       <div class="col-6">
-        <div v-for="upgrade in AppState.clickUpgrades" class="d-flex justify-content-end">
+        <div v-for="upgrade in clickUpgrades" class="d-flex justify-content-end" :key="upgrade.id">
           <button @click="purchaseUpgrade(upgrade)" class="btn btn-primary text-light my-1">{{ upgrade.name }} | +{{
             upgrade.adder }} Cheese | Price: {{ upgrade.price }} | Amount Bought: {{ upgrade.quantity }}</button>
         </div>
       </div>
       <div class="col-6">
-        <div v-for="upgrade in AppState.automaticUpgrades" class="d-flex justify-content-start">
-          <button @click="purchaseUpgrade(upgrade)" class="btn btn-warning text-light my-1">{{ upgrade.name }} | + x{{
-            upgrade.multiplier }} Cheese per second | Price: {{ upgrade.price }} | Amount Bought: {{ upgrade.quantity
-            }}</button>
+        <div v-for="upgrade in autoUpgrades" class="d-flex justify-content-start" :key="upgrade.id">
+          <button @click="purchaseUpgrade(upgrade)" class="btn btn-warning text-light my-1"
+            :disabled="cheese < upgrade.price">
+            {{ upgrade.name }} | + x{{
+              upgrade.multiplier }} Cheese per second | Price: {{ upgrade.price }} | Amount Bought: {{ upgrade.quantity
+            }}
+          </button>
         </div>
       </div>
     </div>
